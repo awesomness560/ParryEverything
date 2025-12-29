b60c197a45436b269f1b940fd8a1afce3328df1f
awesomness560 <appsforyou6@gmail.com>
2025-12-29 01:40:24 -0600

    restart level

--- parry-everything-game/Misc/Signal_Bus.gd
+++ parry-everything-game/Misc/Signal_Bus.gd
@@ -2,3 +2,7 @@ extends Node
 
 signal dealDamage(parryResource : ParryResource)
 signal successfullParry(parryResource : ParryResource, isPerfect : bool)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().change_scene_to_file("res://main.tscn")
